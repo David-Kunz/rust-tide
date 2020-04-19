c3eaec279843e0dd23496958b80c67719a30bbe9
D065023 <david.kunz@sap.com>
2020-04-19 12:08:47 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,56 +5,10 @@ use tide::Server;
 
 mod deserialize_path;
 mod entity;
+mod get_entities;
 
 fn main() -> io::Result<()> {
-    // TODO: Load from external csn file
-    let entities = vec![
-        entity::Entity {
-            name: "entity1".to_string(),
-            elements: vec![
-                entity::Element {
-                    name: "sub11".to_string(),
-                    el_type: entity::ElementType::UUID,
-                    is_key: true,
-                },
-                entity::Element {
-                    name: "sub12".to_string(),
-                    is_key: false,
-                    el_type: entity::ElementType::String,
-                },
-            ],
-        },
-        entity::Entity {
-            name: "entity2".to_string(),
-            elements: vec![
-                entity::Element {
-                    name: "sub21".to_string(),
-                    is_key: true,
-                    el_type: entity::ElementType::UUID,
-                },
-                entity::Element {
-                    name: "sub22".to_string(),
-                    is_key: false,
-                    el_type: entity::ElementType::Integer,
-                },
-            ],
-        },
-        entity::Entity {
-            name: "entity3".to_string(),
-            elements: vec![
-                entity::Element {
-                    name: "sub31".to_string(),
-                    is_key: true,
-                    el_type: entity::ElementType::UUID,
-                },
-                entity::Element {
-                    name: "sub32".to_string(),
-                    is_key: false,
-                    el_type: entity::ElementType::Integer64,
-                },
-            ],
-        },
-    ];
+    let entities = get_entities::get_entities();
     task::block_on(async {
         let mut app = Server::with_state(entities);
 
