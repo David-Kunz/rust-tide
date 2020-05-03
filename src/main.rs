a6235028aae748e0517c9d672067a2486f391a16
D065023 <david.kunz@sap.com>
2020-05-03 14:04:06 +0200

    cosmetics

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,18 @@ use std::env;
 mod add_routes;
 mod cqn;
 mod entity;
-mod parse_csn;
+mod csn;
 mod url_to_cqn;
 
 pub struct State {
-    definitions: parse_csn::Definitions,
+    definitions: csn::Definitions,
     pool: sqlx::SqlitePool,
 }
 
 fn main() -> io::Result<()> {
     task::block_on(async {
         let csn = read_to_string("csn.json").await?;
-        let definitions = parse_csn::Definitions::from_str(&csn).expect("Cannot parse csn");
+        let definitions = csn::Definitions::from_str(&csn).expect("Cannot parse csn");
         let pool = SqlitePool::new(&env::var("DATABASE_URL").unwrap())
             .await
             .unwrap();
